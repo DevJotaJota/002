4ce6f68fa64a51745abc055446cac250c7294aba
João José Chagas Neto <joaojosechagasneto2005@gmail.com>
2024-04-01 15:05:32 -0300

    segundo commit

---
+++ pratica02.js
@@ -0,0 +1,18 @@
+//Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+
+//guardei a informação da variável e perguntei ao usuário usando prompt
+diaDaSemana = prompt('Qual é o dia da semana?');
+
+//estrutura de condição
+if(diaDaSemana == 'Sábado')
+    {
+        alert('Bom fim de semana!');
+    }
+    else if(diaDaSemana == 'Domingo')
+        {
+            alert('Bom fim de semana!');
+        }
+        else
+            {
+                alert('Boa semana!');
+            }
